74725aeb7797d2b5fa69197068d82974c213ff77
peondusud <reflexion_31@hotmail.com>
2015-08-06 01:55:23 +0200

    python script minimal wokring

--- leboncoin.py
+++ leboncoin.py
@@ -6,11 +6,11 @@ import os
 import sys
 import re
 import requests
-import lxml #apt-get install libxml2-dev libxslt-dev python-dev lib32z1-dev
+from lxml import html	 #apt-get install libxml2-dev libxslt-dev python-dev lib32z1-dev
 import threading
 
-import tornado.ioloop
-import tornado.web
+#import tornado.ioloop
+#import tornado.web
 
 
 start_urls=[]
@@ -32,16 +32,24 @@ def check_domain(url_to_check):
 
 
 def fetch():
-    r = requests.get("http://leboncoin.fr/")
-    print(r.text )
-    print  r.text          #Retourne le contenu en unicode
-    print  r.content       #Retourne le contenu en bytes
-    print  r.json          #Retourne le contenu sous forme json
-    print  r.headers       #Retourne le headers sous forme de dictionnaire
-    print  r.status_code   #Retourne le status code
-    print response.request.headers # Headers you sent with the request 
+    r = requests.get('http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions/')
+    #print  r.text          #Retourne le contenu en unicode
+    #print  r.content       #Retourne le contenu en bytes
+    #print  r.json          #Retourne le contenu sous forme json
+    #print  r.headers       #Retourne le headers sous forme de dictionnaire
+    #print  r.status_code   #Retourne le status code
+    #print  r.request.headers # Headers you sent with the request 
+    
 
-    check_domain()
+    tree = html.fromstring( r.text )
+    prev_nxt_url = tree.xpath('/html/body/div[@id="page_align"]/div[@id="page_width"]/div[@id="ContainerMain"]/nav/ul[@id="paging"]//li[@class="page"]/a/@href')
+    next_url = prev_nxt_url[-1]
+    nb_page = next_url.split("?o=")[-1]
+
+    print next_url
+    print nb_page
+
+    check_domain(next_url)
 
 def parse():
     pass
@@ -59,6 +67,6 @@ def stats():
     pass
 
 if __name__ == '__main__':
-    pass
+   fetch() 
 
 
